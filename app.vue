7787b2290eb0ebc8a68b59e09f8592e7a9bc4548
garyellow <gary20011110@gmail.com>
2023-02-01 00:32:55 +0800

    Test

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
-    <NuxtPage />
+    <!-- <NuxtPage /> -->
+    <NuxtWelcome />
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,5 +2,6 @@
   <div>
     <h1>Index Page</h1>
     <p>My first Nuxt.js app</p>
+    <p>abc</p>
   </div>
 </template>
