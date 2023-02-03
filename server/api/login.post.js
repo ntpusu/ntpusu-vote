7f145df8597c1ee0a112da3ee0f1dd4eaa8e8e87
garyellow <gary20011110@gmail.com>
2023-02-04 01:37:19 +0800

    Test

--- server/api/login.post.js
+++ server/api/login.post.js
@@ -1,23 +1,29 @@
 import AES from 'crypto-js/aes'
 export default defineEventHandler(async (event) => {
-    const { username, password } = await readBody(event)
+    try {
+        const { username, password } = await readBody(event)
 
-    const payload = 'stud_num=' + username + '&passwd=' + password + '&x=0&y=0'
+        const payload = 'stud_num=' + username + '&passwd=' + password + '&x=0&y=0'
 
-    const res = await fetch(process.env.STUDENT_SYSTEM_URL, {
-        method: 'POST',
-        body: payload
-    }).then(res => res.text())
+        const res = await fetch(process.env.STUDENT_SYSTEM_URL, {
+            method: 'POST',
+            body: payload
+        }).then(res => res.text())
 
-    const login_state = res.startsWith('<body onload="window.open(\'../univer/query_all_course.login2?date1=')
+        const login_state = res.startsWith('<body onload="window.open(\'../univer/query_all_course.login2?date1=')
 
-    if (login_state) {
-        setCookie(event, 'un', AES.encrypt(username, process.env.CRYPTO_KEY).toString(), {
-            httpOnly: true,
-            secure: true,
-            maxAge: 600
-        })
+        if (login_state) {
+            setCookie(event, 'un', AES.encrypt(username, process.env.CRYPTO_KEY).toString(), {
+                httpOnly: true,
+                secure: true,
+                maxAge: 600
+            })
+        }
+
+        return login_state
     }
-
-    return login_state
+    catch (err) {
+        console.log(err)
+        return false
+    }
 })
