f51726668efd61d347ffdd359812cf934df02048
garyellow <gary20011110@gmail.com>
2023-02-04 15:44:00 +0800

    Add 404 page

---
+++ pages/[...slug].vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="content">
+        <h1>404 NOT FOUND!!!</h1>
+        <h5 v-if="countdown <= 5">{{ countdown.toFixed(1) }}秒後自動跳轉至首頁</h5>
+    </div>
+</template>
+
+<script lang="ts" setup>
+setResponseStatus(404)
+
+const countdown = ref(10)
+const timer = () => {
+    console.log(countdown.value)
+    setTimeout(() => {
+        countdown.value -= 0.1
+
+        if (countdown.value > 0.1) {
+            timer()
+        }
+        else {
+            useRouter().push('/')
+        }
+    }, 100)
+}
+
+onMounted(() => {
+    timer()
+})
+</script>
+
+<style scoped>
+.content {
+    text-align: center;
+    margin: auto;
+}
+</style>
