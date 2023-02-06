11f3bfc707e00f68250a594d09f2dfcac579e26e
garyellow <gary20011110@gmail.com>
2023-02-06 22:29:43 +0800

    Lint app.vue code

--- app.vue
+++ app.vue
@@ -1,36 +1,44 @@
 <template>
-  <NuxtLoadingIndicator />
-  <div>
-    <el-container>
-      <el-header>
-        <ClientOnly>
-          <template #fallback>
-            <div style="text-align: center;">
-              Loading menu......
-            </div>
-          </template>
-          <el-menu :default-active="$route.path" :unique-opened="true" mode="horizontal" :router="true"
-            :ellipsis="false" @select="handleSelect">
-            <h1 class="margin">&nbsp;投票系統</h1>
-            <div class="flex-grow" />
-            <el-menu-item index="/">首頁</el-menu-item>
-            <el-menu-item v-if="loginState" index="/allVote">投票清單</el-menu-item>
-            <el-menu-item v-if="!loginState" index="/login">登入</el-menu-item>
-            <el-menu-item v-else index="/logout">登出</el-menu-item>
-          </el-menu>
-        </ClientOnly>
-      </el-header>
-      <el-main>
-        <NuxtPage />
-      </el-main>
-      <!-- <el-footer></el-footer> -->
-    </el-container>
-  </div>
-  <el-backtop />
+    <NuxtLoadingIndicator />
+    <div>
+        <el-container>
+            <el-header>
+                <ClientOnly>
+                    <template #fallback>
+                        <div style="text-align: center">Loading menu......</div>
+                    </template>
+                    <el-menu
+                        :default-active="$route.path"
+                        :unique-opened="true"
+                        mode="horizontal"
+                        :router="true"
+                        :ellipsis="false"
+                        @select="handleSelect"
+                    >
+                        <h1 class="margin">&nbsp;投票系統</h1>
+                        <div class="flex-grow" />
+                        <el-menu-item index="/">首頁</el-menu-item>
+                        <el-menu-item v-if="loginState" index="/allVote"
+                            >投票清單</el-menu-item
+                        >
+                        <el-menu-item v-if="!loginState" index="/login"
+                            >登入</el-menu-item
+                        >
+                        <el-menu-item v-else index="/logout">登出</el-menu-item>
+                    </el-menu>
+                </ClientOnly>
+            </el-header>
+            <el-main>
+                <NuxtPage />
+            </el-main>
+            <!-- <el-footer></el-footer> -->
+        </el-container>
+    </div>
+    <el-backtop />
 </template>
 
 <script lang="ts" setup>
-import { Ref } from "vue"
+import { Ref } from 'vue'
 
 const curIndex = ref('1')
 const loginState = useState('loginState') as Ref<boolean>
@@ -38,12 +46,12 @@ const loginState = useState('loginState') as Ref<boolean>
 const activeIndex = () => curIndex.value
 
 const handleSelect = (key: string, keyPath: string[]) => {
-  curIndex.value = key
+    curIndex.value = key
 }
 </script>
 
 <style>
 .flex-grow {
-  flex-grow: 1;
+    flex-grow: 1;
 }
 </style>
