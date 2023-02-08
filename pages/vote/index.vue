1fee1b2425593eecab022437e2407414471ff820
garyellow <gary20011110@gmail.com>
2023-02-08 19:52:06 +0800

    Add form for vottimg

--- pages/vote/index.vue
+++ pages/vote/index.vue
@@ -1,44 +1,68 @@
 <template>
     <el-space v-if="!VSPending" class="justify-center" wrap>
-        <el-card v-for="VSitem in VS" :key="VSitem.id" style="width: 400px">
+        <el-card
+            v-for="(VSitem, index) in VS"
+            :key="index"
+            shadow="hover"
+            class="w-[25rem]"
+        >
             <template #header>
                 <el-space class="!flex justify-between">
-                    <div class="text-xl font-bold">
+                    <div class="flex-wrap text-xl font-bold">
                         {{ VSitem.name }}
                     </div>
                     <div>
-                        <div class="text-xs">
+                        <div class="w-44 text-[0.7rem] leading-4">
                             開始：{{ viewDate(VSitem.startTime) }}
                         </div>
-                        <div class="text-xs">
+                        <div class="w-44 text-[0.7rem] leading-4">
                             截止：{{ viewDate(VSitem.endTime) }}
                         </div>
                     </div>
                 </el-space>
             </template>
-            <dix>
+            <div v-if="timeCnt(VSitem.startTime) > Date.now()">
                 <div
-                    v-for="(candidate, index) in VSitem.candidates"
-                    :key="index"
+                    v-for="(candidate, itemIndex) in VSitem.candidates"
+                    :key="itemIndex"
                 >
-                    {{ index + 1 }}. {{ candidate.name }}
+                    {{ candidate.name }}
                 </div>
-            </dix>
-            <el-divider border-style="dotted" />
-            <div class="text px-32">
-                <el-button
-                    v-if="timeCnt(VSitem.endTime) < Date.now()"
-                    type="primary"
-                    class="w-full tracking-widest"
-                    @click="useRouter().push('/vote/' + VSitem.id)"
-                    >結果
-                </el-button>
-                <el-button
-                    v-else-if="timeCnt(VSitem.startTime) < Date.now()"
-                    type="primary"
-                    class="w-full tracking-widest"
-                    >投票
-                </el-button>
+            </div>
+            <div v-else>
+                <el-form :model="voteData">
+                    <el-radio-group
+                        class="!grid justify-start"
+                        v-model="voteData.selected[index]"
+                        :disabled="timeCnt(VSitem.endTime) < Date.now()"
+                    >
+                        <el-radio
+                            v-for="(candidate, itemIndex) in VSitem.candidates"
+                            :key="itemIndex"
+                            :label="candidate.id"
+                            border
+                            class="!mr-0 mb-2"
+                            >{{ candidate.name }}
+                        </el-radio>
+                    </el-radio-group>
+                    <el-divider border-style="dashed" />
+                    <div class="text px-32">
+                        <NuxtLink
+                            v-if="timeCnt(VSitem.endTime) < Date.now()"
+                            class="inline-flex w-full justify-center rounded-md bg-green-600 px-6 py-1.5 text-sm tracking-widest text-white hover:bg-green-500"
+                            :to="'/vote/' + VSitem.id"
+                        >
+                            結果
+                        </NuxtLink>
+                        <el-button
+                            v-else
+                            type="primary"
+                            class="w-full !rounded-md tracking-widest"
+                            @click="voteConfirm(index)"
+                            >投票
+                        </el-button>
+                    </div>
+                </el-form>
             </div>
         </el-card>
     </el-space>
@@ -46,7 +70,7 @@
 </template>
 
 <script lang="ts" setup>
-import type { FormInstance, FormRules } from 'element-plus'
+import type { Candidate } from '@prisma/client'
 
 definePageMeta({
     middleware: ['auth'],
@@ -70,11 +94,40 @@ const timeCnt = (time: Date) => {
     return newDate(time).getTime()
 }
 
-const formRef = ref<FormInstance>()
+const voteData = reactive<{ selected: number[] }>({ selected: [] })
 
-const voteData = reactive<{
-    selected: number
-}>({
-    selected: 0,
-})
+const voteConfirm = async (index: number) => {
+    if (
+        voteData.selected[index] !== undefined &&
+        voteData.selected[index] !== null
+    ) {
+        const candidate = (await $fetch(
+            '/api/uniCa' +
+                '?' +
+                new URLSearchParams({ id: voteData.selected[index].toString() })
+        )) as Candidate | null
+
+        ElMessageBox.confirm(
+            '確定要投給「' + candidate?.name + '」嗎？',
+            '再次確認',
+            {
+                confirmButtonText: '確定',
+                cancelButtonText: '取消',
+                type: 'warning',
+            }
+        )
+            .then(() => {
+                ElMessage({
+                    type: 'success',
+                    message: 'Delete completed',
+                })
+            })
+            .catch(() => {
+                ElMessage({
+                    type: 'info',
+                    message: 'Delete canceled',
+                })
+            })
+    }
+}
 </script>
