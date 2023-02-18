dca2e5dfe33d685ffa23ad97077bbaa5eaaaabfa
garyellow <gary20011110@gmail.com>
2023-02-18 20:22:19 +0800

    Add accelerate

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,6 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from '@prisma/client';
+import useAccelerate from '@prisma/extension-accelerate';
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient().$extends(useAccelerate)
 
 export default prisma
