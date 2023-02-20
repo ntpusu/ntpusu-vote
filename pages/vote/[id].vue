8c9583eae32e642800aa581fd0882aada885ed1e
garyellow <gary20011110@gmail.com>
2023-02-20 16:40:13 +0800

    Add final page

--- pages/vote/[id].vue
+++ pages/vote/[id].vue
@@ -1,9 +1,71 @@
-<template></template>
+<template>
+    <div v-if="!VSPending">
+        <el-space
+            v-if="timeCnt(VS.endTime) < Date.now()"
+            class="justify-center"
+        >
+            <el-card
+                v-for="(Candidate, index) in VS.candidates"
+                :key="index"
+                shadow="hover"
+                class="w-96"
+            >
+                <template #header>
+                    <div class="flex-wrap text-xl font-bold">
+                        {{ Candidate.name }}
+                    </div>
+                </template>
+                <div
+                    v-for="(Ballot, index) in Candidate.ballots"
+                    :key="index"
+                    class="flex break-all"
+                >
+                    <div>{{ index + 1 }}.&nbsp;</div>
+                    <div>{{ Ballot.token }}</div>
+                </div>
+            </el-card>
+        </el-space>
+    </div>
+    <el-empty v-else description="loading......" />
+</template>
 
 <script lang="ts" setup>
 definePageMeta({
     middleware: ['auth'],
 })
 
-const { id } = useRoute().params
+const { id } = useRoute().params as { id: string }
+
+const {
+    data: VS,
+    pending: VSPending,
+    refresh: VSRefresh,
+} = useFetch('/api/uniVsCaBa?' + new URLSearchParams({ id })) as unknown as {
+    data: {
+        id: string
+        name: string
+        endTime: Date
+        candidates: {
+            id: string
+            name: string
+            ballots: {
+                token: string
+            }[]
+        }[]
+    }
+    pending: boolean
+    refresh: () => void
+}
+
+const newDate = (time: Date) => {
+    return new Date(time)
+}
+
+const viewDate = (time: Date) => {
+    return newDate(time).toLocaleString()
+}
+
+const timeCnt = (time: Date) => {
+    return newDate(time).getTime()
+}
 </script>
