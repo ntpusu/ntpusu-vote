8fc60ab708c21b182d27a23226fe5ff1a2c30f33
garyellow <gary20011110@gmail.com>
2023-02-23 16:00:40 +0800

    Del unused code

--- pages/admin.vue
+++ pages/admin.vue
@@ -4,7 +4,7 @@
             class="m-auto w-full rounded-xl border-4 border-blue-100 p-5 sm:w-7/12 md:w-1/2 lg:w-5/12 xl:w-1/3 2xl:w-1/4"
         >
             <el-form
-                :label-width="'auto'"
+                label-width="auto"
                 label-suffix=":"
                 ref="formRef"
                 :model="addVote"
