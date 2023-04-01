093bc3b28cab80cb2b1c80a5898efb1eaefef8c1
garyellow <gary20011110@gmail.com>
2023-04-01 16:05:05 +0800

    Prevent hot reloading from creating new instance

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,3 +5,5 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient }
 export const prisma = globalForPrisma.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+export default prisma
