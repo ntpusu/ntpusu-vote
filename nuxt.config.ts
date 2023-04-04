b82bdb5bca733ba8efee512219dae2aee04d17d3
garyellow <gary20011110@gmail.com>
2023-04-04 20:05:42 +0800

    Change env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,7 @@ export default defineNuxtConfig({
         // The module is enabled. Change this to disable the module
         isEnabled: true,
         // The origin is set to the development origin. Change this when deploying to production by setting `origin` in this config before build-time or by exporting `AUTH_ORIGIN` by running `export AUTH_ORIGIN=...`
-        origin: 'http://localhost:3000',
+        origin: process.env.ORIGIN,
         // The base path to the authentication endpoints. Change this if you want to add your auth-endpoints at a non-default location
         basePath: '/api/auth',
         // Whether to periodically refresh the session. Change this to `true` for a refresh every seconds or set this to a number like `5000` for a refresh every 5000 milliseconds (aka: 5 seconds)
