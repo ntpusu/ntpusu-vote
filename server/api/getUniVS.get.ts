0f68007edd7c1d9159bde190b195e3d2035fe29a
garyellow <gary20011110@gmail.com>
2023-04-05 19:07:56 +0800

    Add new api to result

---
+++ server/api/getUniVS.get.ts
@@ -0,0 +1,61 @@
+import prisma from '~/lib/prisma'
+import { getServerSession } from '#auth'
+export default defineEventHandler(async (event) => {
+    const session = await getServerSession(event) as { user: { email: string } } | null
+
+    if (!session) {
+        throw createError({
+            statusCode: 401,
+            statusMessage: 'Unauthorized'
+        })
+    }
+
+    const email = session['user']['email']
+    const studentId = email.substring(1, 10)
+
+    const voter = await prisma.voter.findUnique({
+        where: { id: parseInt(studentId) },
+        include: { VoterInGroup: true },
+    })
+
+    if (!voter) {
+        throw createError({
+            statusCode: 401,
+            statusMessage: 'Unauthorized'
+        })
+    }
+
+
+    const { id } = getQuery(event) as { id: string }
+
+    const groupIds = voter.VoterInGroup.map((item) => item.groupId)
+
+    const VS = await prisma.voteSession.findUnique({
+        where: {
+            id: parseInt(id),
+        },
+        include: {
+            candidates: {
+                include: {
+                    ballots: true,
+                },
+            },
+        },
+    })
+
+    if (!VS) {
+        throw createError({
+            statusCode: 404,
+            statusMessage: 'Not Found'
+        })
+    }
+
+    if (!groupIds.includes(VS.groupId)) {
+        throw createError({
+            statusCode: 401,
+            statusMessage: 'Unauthorized'
+        })
+    }
+
+    return VS
+})
