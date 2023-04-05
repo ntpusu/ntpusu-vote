0f4e3bbfefbe690d3ae6ed5afb735a3af6537428
garyellow <gary20011110@gmail.com>
2023-04-05 16:16:53 +0800

    Update main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
-    <div></div>
+    <h1 class="text-center text-4xl font-black">就是首頁</h1>
+    <NuxtLink to="/vote" class="text-center text-xl font-black"
+        >前往投票</NuxtLink
+    >
 </template>
 
 <script lang="ts" setup>
