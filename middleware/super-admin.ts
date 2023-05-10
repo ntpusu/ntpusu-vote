95ab29942dd2f550de2df9b6b3f69a3c91e755e4
garyellow <gary20011110@gmail.com>
2023-05-11 01:22:25 +0800

    Change middleware auth

--- middleware/super-admin.ts
+++ middleware/super-admin.ts
@@ -1,23 +1,12 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
-    const { data } = useAuth()
+    if (process.client) {
+        const { data: admin } = await useFetch('/api/checkSuperAdmin')
 
-    if (!data) {
-        return await navigateTo('/404')
+        if (!admin) {
+            return await navigateTo('/404')
+        }
     }
-
-    const email = data.value?.user?.email
-
-    if (!email) {
-        return await navigateTo('/404')
-    }
-
-    const id = email.substring(1, 10)
-    const { data: admin } = await useFetch('/api/checkSuperAdmin', {
-        method: 'POST',
-        body: JSON.stringify({ id })
-    })
-
-    if (!admin) {
+    else {
         return await navigateTo('/404')
     }
 })
