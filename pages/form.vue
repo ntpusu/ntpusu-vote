1107a09037031d0e9848389ec9920585845c5914
garyellow <gary20011110@gmail.com>
2023-05-11 21:59:37 +0800

    Try another

--- pages/form.vue
+++ pages/form.vue
@@ -1,6 +1,6 @@
 <template>
     <iframe
-        src="https://docs.google.com/forms/d/e/1FAIpQLSeNt5WIb7hMu7UT1avB-X2I_JiaNij2GLwuwdY_wNtGn6xoWA/viewform?embedded=true"
+        src="https://docs.google.com/forms/d/e/1FAIpQLSeHDcfLkA5Lu5oJLpNTgwwP48AjKpdXbf-HrIzXyJfU-1YZIg/viewform?embedded=true"
         width="640"
         height="1000"
         frameborder="0"
