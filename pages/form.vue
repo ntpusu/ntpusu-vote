aa6cc316e6479c5204ee6ab4e4a8c3cdd0674f43
garyellow <gary20011110@gmail.com>
2023-05-15 14:06:16 +0800

    UI Update

--- pages/form.vue
+++ pages/form.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="flex h-[80vh] items-center justify-center">
+    <div class="flex h-[80vh] items-center justify-center p-16">
         <iframe
             src="https://docs.google.com/forms/d/e/1FAIpQLSeNt5WIb7hMu7UT1avB-X2I_JiaNij2GLwuwdY_wNtGn6xoWA/viewform?embedded=true"
             width="100%"
-            height="300"
             frameborder="0"
             marginheight="0"
             marginwidth="0"
             loading="lazy"
+            style="height: 100%"
         >
             載入中......
         </iframe>
