590dddc7aa1b904eef9c07aa4e0af08e4d431ac2
garyellow <gary20011110@gmail.com>
2023-05-22 10:26:33 +0800

    Modify limiter

--- server/middleware/limiter.ts
+++ server/middleware/limiter.ts
@@ -1,6 +1,6 @@
 import { RateLimiter } from "limiter";
 
-const tokensPerInterval = 150
+const tokensPerInterval = 300
 const interval = 'hour'
 const limiter = new RateLimiter({
     tokensPerInterval,
@@ -21,22 +21,36 @@ const limitList = [
     '/api/checkLogin',
     '/api/getBallot',
     '/api/getGroup',
-    '/api/getLoginCnt',
     '/api/getVoter',
     '/api/getVS',
     '/api/resetLoginCnt',
     '/api/unarchiveVS',
 ]
 
+const subLimitlist = [
+    '/api/addAdmin',
+    '/api/checkSuperAdmin',
+    '/api/delAdmin',
+    '/api/delVS',
+    '/api/getAdmin',
+    '/api/getLoginCnt',
+]
+
 export default defineEventHandler(async (event) => {
     if (event.node.req.url) {
         if (superLimitList.includes(event.node.req.url)) {
-            if (await limiter.removeTokens(5) < 0) {
+            if (await limiter.removeTokens(10) < 0) {
                 event.node.res.writeHead(429, { 'Content-Type': 'text/plain;charset=UTF-8' })
                 event.node.res.end('429 Too Many Requests - your IP is being rate limited')
             }
         }
         else if (limitList.includes(event.node.req.url)) {
+            if (await limiter.removeTokens(3) < 0) {
+                event.node.res.writeHead(429, { 'Content-Type': 'text/plain;charset=UTF-8' })
+                event.node.res.end('429 Too Many Requests - your IP is being rate limited')
+            }
+        }
+        else if (subLimitlist.includes(event.node.req.url)) {
             if (await limiter.removeTokens(1) < 0) {
                 event.node.res.writeHead(429, { 'Content-Type': 'text/plain;charset=UTF-8' })
                 event.node.res.end('429 Too Many Requests - your IP is being rate limited')
