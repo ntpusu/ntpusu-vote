49b3af280ed66fa3a00de6e542d8484c689e879d
許君愷 / xu jun kai <kevin16021777@gmail.com>
2024-04-01 12:38:44 +0000

    Add new dependencies and update upload functionality

--- pages/manage_electoral_district.vue
+++ pages/manage_electoral_district.vue
@@ -6,10 +6,12 @@
         <el-upload
         ref="uploadRef"
         class="upload-demo"
-        action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+        action="none"
         accept=".xlsx"
+        :http-request="uploadfunc"
         :drag="true"
         :auto-upload="false"
+        :limit="1"
         >
         <!-- 上傳按鈕 -->
         <template #trigger>
@@ -30,6 +32,49 @@
         </el-upload>
         <!-- 顯示目前系統內的資料筆數 -->
         <el-text class="mx-1" size="large">目前系統內有{{electorCount}}筆資料</el-text>
+    <el-dialog
+            v-model="uploadDialogVisible"
+            title="設定上傳模式"
+            width="500"
+            align-center
+        >
+        <el-select
+            v-model="seletingUploadMode"
+            placeholder="新增"
+            size="large"
+            style="width: 240px"
+        >
+            <el-option
+                v-for="item in uploadModes"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+            />
+        </el-select>
+        <br>
+            <div class="dialog-footer">
+                <el-button @click="uploadDialogVisible = false">取消</el-button>
+                <el-button type="primary" @click="uploadSubmit">
+                    確認上傳
+                </el-button>
+            </div>
+        </el-dialog>
+        <br>
+        <br>
+
+        <el-dialog
+            v-model="dataChangedialogVisible"
+            title="更動頁面"
+            width="500"
+        >
+            <template #footer>
+            <div class="dialog-footer">
+                <el-button type="primary" @click="dialogVisible = false">
+                    關閉頁面
+                </el-button>
+            </div>
+            </template>
+        </el-dialog>
     </div>
 </template>
 
@@ -37,6 +82,24 @@
 // 引入 vue 和 element-plus 的 UploadInstance
 import { ref } from 'vue'
 import type { UploadInstance } from 'element-plus'
+import { Search } from '@element-plus/icons-vue'
+
+const dataChangedialogVisible = ref(false)
+const uploadDialogVisible = ref(false)
+const queryInput = ref('')
+const modifydepartmentInput = ref('')
+const studentIdStatus = ref(0) /* 0: no input, 1 : not found, 2 : found data */
+const seletingUploadMode = ref('')
+let queryInputData = ''
+
+const GroupData: Ref<{
+    id: number
+    Int: number
+    first: {
+        serNum: number | null
+        time: number | null
+    }
+} | null> = ref(null)
 
 // 定義頁面元資訊
 definePageMeta({
@@ -56,9 +119,81 @@ const submitUpload = () => {
 const {
     data: electorCount,
     //pending: adminPending,
-    //refresh: adminRefresh,
-} = await useLazyFetch('/api/getAllElector')
+    refresh: electorCountRefresh,
+} = await useLazyFetch('/api/getAllElectorCnt')
+//1. add api_getAllElectorCnt
 
 //const t = await useLazyFetch('/api/getAllVoter')
+
+const uploadfunc = async (item) => {
+    const file = item.file as File
+    const fileType = file.name.substring(file.name.lastIndexOf('.'))
+    if (fileType !== '.xlsx') {
+        ElMessage.error('錯誤:僅能上傳.xlsx文件')
+        uploadRef.value!.clearFiles()
+        return false
+    }
+    //2. add api_deleteAllgroup  ok
+    if (seletingUploadMode.value == 'override') {
+        await useFetch('/api/delAllGroup', {
+            method: 'DELETE',
+        })
+    }
+    let formData = new FormData()
+
+    formData.append("fileName", file.name)
+    formData.append("file", file)
+
+    //3. add api_upload  ok
+    const {
+        data: uploadingFileStatus,
+    } = await useFetch('/api/uploadGroup', {
+            method: 'POST',
+            body: formData,
+        })
+    //console.log(uploadingFileName)
+    ElMessage.success('上傳成功' + " " + uploadingFileStatus)
+    electorCountRefresh()
+    uploadRef.value!.clearFiles()
+}
+//4. set api_getGroup
+const queryStudentData = async() => {
+    const res = await useFetch('/api/getGroup', {
+        method: 'GET',
+        query: { voter: parseInt(queryInput.value)},
+    })
+    if (res.error.value) {
+        studentIdStatus.value = 1;
+        return;
+    }
+    voterData.value = res.data.value
+    queryInputData = queryInput.value
+    studentIdStatus.value = 2;
+}
+
+const midifyDepartment = async () => {
+
+}
+//5. add api_delgroup
+const deleteVoterData = async () => {
+    await useFetch('/api/delgroup', {
+        method: 'DELETE',
+        query: { id: queryInputData },
+    })
+        .catch(() => {
+            ElMessage.error('刪除失敗')
+        })
+        .finally(() => {
+            queryInput.value = ''
+            studentIdStatus.value = 0
+            electorCountRefresh()
+        })
+}
+
+const uploadSubmit = async () => {
+    submitUpload();
+    uploadDialogVisible.value = false;
+}
+//
 
 </script>
