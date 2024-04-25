3481a9a13fa23c1f137203568a2beb8741d63023
TonyLiu0430 <tony04302020@gmail.com>
2024-04-25 21:58:39 +0800

    fix: timeout by thread

--- server/api/uploadVoter.post.ts
+++ server/api/uploadVoter.post.ts
@@ -36,9 +36,26 @@ export default defineEventHandler(async (event) => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const voter_table: any[][] = XLSX.utils.sheet_to_json(voter_sheet, { header: 1 })
 
-    const failAddingVoter: {
-        id: number,
-    }[] = [];
+    const tasks: Promise<void>[] = []
+    for(let i = 1; i < voter_table.length; i += 100) {
+        tasks.push(task(voter_table.slice(i, min(voter_table.length, i + 100))));
+    }
+
+    await Promise.all(tasks)
+
+    return failAddingVoter;
+/*
+    const tasks = [
+        task(voter_table),
+        task(voter_table),
+        task(voter_table),
+        task(voter_table),
+        task(voter_table)
+    ]
+
+    await Promise.all(tasks)
+    
+    await task(voter_table)
 
     for (let i = 1; i < voter_table.length; i++) {
         const studentId = voter_table[i][0] as number
@@ -72,6 +89,49 @@ export default defineEventHandler(async (event) => {
             })
         }
     }
-
-    return failAddingVoter;
+*/
+    
 })
+
+const min = (a, b) => {
+    return a > b ? b : a;
+}
+
+const failAddingVoter: {
+    id: number,
+}[] = [];
+
+async function task(data:any[][]) {
+    for (let i = 1; i < data.length; i++) {
+        const studentId = data[i][0] as number
+        //const studentName = data[i][1] as string
+        const studentDepartment = (data[i][2] as string).replace(/\d[AB]?/, "")
+        const department = await prisma.department.findUnique({
+            where: {
+                name: studentDepartment,
+            },
+            select: {
+                id: true,
+            },
+        })
+        if (department !== null) {
+            await prisma.voter.upsert({
+                where: {
+                    id: studentId,
+                },
+                update: {
+                    departmentId: department.id,
+                },
+                create: {
+                    id: studentId,
+                    departmentId: department.id,
+                },
+            })
+        }
+        else {
+            failAddingVoter.push({
+                id: studentId,
+            })
+        }
+    }
+}
