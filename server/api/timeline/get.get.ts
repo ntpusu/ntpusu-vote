b22dc4af6b125f7bd5344140400fcec0d13079cf
312432 <andy.wu.cyut.edu@gmail.com>
2026-05-07 08:48:01 +0000

    fix: month display error

--- server/api/timeline/get.get.ts
+++ server/api/timeline/get.get.ts
@@ -13,9 +13,9 @@ export default defineEventHandler(async (event) => {
         if (!timelines) {
             return "(無抽獎)"
         }
-        const timeString = 
-        timelines.start.getMonth() +"月"+ timelines.start.getDate()+ "日" + " " + timelines.start.getHours() + ":" + String(timelines.start.getMinutes()).padStart(2, '0') +
-        " 至 " + timelines.end.getMonth() +"月"+ timelines.end.getDate()+ "日" + " " + timelines.end.getHours() + ":" + String(timelines.end.getMinutes()).padStart(2, '0')
+        const timeString =
+        timelines.start.getMonth()+1 +"月"+ timelines.start.getDate()+ "日" + " " + timelines.start.getHours() + ":" + String(timelines.start.getMinutes()).padStart(2, '0') +
+        " 至 " + timelines.end.getMonth()+1 +"月"+ timelines.end.getDate()+ "日" + " " + timelines.end.getHours() + ":" + String(timelines.end.getMinutes()).padStart(2, '0')
         return timeString
     }
     return await prisma.votingTimeline.findMany({
